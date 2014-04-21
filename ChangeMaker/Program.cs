9db15027fb5cdf91524f506b3a082d329f3218b7
SeedPaths-User <lessdismissive@email.com>
2014-04-21 14:52:21 -0600

    ChangeMaker
    changemakeronline

---
+++ ChangeMaker/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            ChangeMaker(3.18);
+            Console.WriteLine();
+            ChangeMaker(0.99);
+            Console.WriteLine();
+            ChangeMaker(12.93);
+            Console.WriteLine();
+
+
+
+
+            Console.ReadKey();
+
+        }
+        // creating function called ChangeMaker
+        static void ChangeMaker(double amount)
+        {
+           
+            Console.WriteLine("Amount: "+ amount);
+            
+            var Quarters = 0;
+            var Dimes = 0;
+            var Nickels = 0;
+            var Pennies = 0;
+
+            while (amount > .25)
+            {
+                Quarters++;
+                amount -= .25;
+            }
+            while (amount > .10)
+            {
+                Dimes++;
+                amount -= .10;
+            }
+            while (amount > .05)
+            {
+                Nickels++;
+               amount -= .05;
+            }
+            while (amount > .01)
+            {
+                Pennies++;
+                amount -= .01;
+            }
+
+            Console.WriteLine("Amount of Quarters: " + Quarters);
+            Console.WriteLine("Amount of Dimes: " + Dimes);
+            Console.WriteLine("Amount of Nickels: " + Nickels);
+            Console.WriteLine("Amount of Pennies: " + Pennies);
+            Console.WriteLine("Total Amount of Coins: " + amount);
+        }
+
+    }
+}
---
+++ ChangeMakerrr/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeMakerrr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+            //Call the function with the parameters
+
+
+
+
+
+            //Making ChangeMaker function
+
+
+
+            //Print amount first, as it is modified in the code
+
+
+            //Declaring variables
+
+
+            //While amount is more than .25, it takes off a quarter from the amount and adds it to the quarter vaiable
+
+
+
+            //While amount is more than .10, it takes off a dime from the amount and adds it to the dime variable
+
+
+
+
+            //While amount is more than .05, it takes off a nickel from the amount and adds it to the nickel variable
+
+
+            //While amount is more than .01, it takes off a penny from the amount and adds it to the penny variable
+
+
+            //Print amount of quarters, dimes, nickels, and pennys.
+        
+    }
+}
